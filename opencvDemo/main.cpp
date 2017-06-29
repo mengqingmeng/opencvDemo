1fa73e67aa9b4107de850768219d2123bc4523de
mqm <762577099@qq.com>
2017-06-29 22:40:15 +0800

    Initial Commit

---
+++ opencvDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvDemo
+//
+//  Created by mqm on 2017/6/29.
+//  Copyright © 2017年 mqm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
