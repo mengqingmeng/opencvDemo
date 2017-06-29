985d055ccf6d420c576c3f07582734a969fc66af
mqm <762577099@qq.com>
2017-06-30 00:16:22 +0800

    init

--- opencvDemo/main.cpp
+++ opencvDemo/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+  
     return 0;
 }
